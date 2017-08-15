579024c469291e836b9f3b18f94c3a5e8bfcf72d
Mitchell Chung <mchung94@gmail.com>
2017-08-15 15:42:13 -0700

    Better software module spec and error handling
    Software modules should raise an Exception if there's a problem while
    determining installed_version and latest_version.  Then they'll be
    listed as 'Unknown' in the report.
    
    Also, it now pre-checks software modules to make sure they really
    have the functions name(), installed_version(), and latest_version()
    before reporting on them.

--- versions/latest.py
+++ versions/latest.py
@@ -1,34 +1,63 @@
-import collections
 import pkgutil
 
 import versions.software
 
 
-VersionInfo = collections.namedtuple('VersionInfo', ['name', 'installed_version', 'latest_version'])
+def all_modules(package_path):
+    """"Return a list of all modules in the given package path."""
+
+    def load_module(module_info):
+        """Load and return the module referred to by the given ModuleInfo."""
+        return module_info.module_finder.find_module(module_info.name).load_module(module_info.name)
+
+    return [load_module(m_info) for m_info in pkgutil.iter_modules(package_path) if not m_info.ispkg]
 
 
-def get_version_info(module_info):
-    module_finder, name, _ = module_info
-    m = module_finder.find_module(name).load_module(name)
-    return VersionInfo(m.name(), m.installed_version(), m.latest_version())
+def is_software_module(m):
+    """Return True if the given module m has name(), installed_version(), and latest_version() defined."""
+
+    def has_callable(name):
+        """Return True if the module m has a callable by the given name."""
+        try:
+            attr = getattr(m, name)
+            return callable(attr)
+        except AttributeError:
+            return False
+
+    def has_callables(*args):
+        """Return True if all the args are names of functions defined in module m."""
+        return all([has_callable(attr) for attr in args])
+
+    return has_callables('name', 'installed_version', 'latest_version')
 
 
-def all_versions():
-    return (get_version_info(m) for m in pkgutil.iter_modules(versions.software.__path__) if not m.ispkg)
-
-
-def print_version_results(version_info):
-    name, installed_version, latest_version = version_info
+def print_version_results(software_module):
+    """Print a human-readable report on the software's name/current/latest version."""
+    name = software_module.name()
+    # noinspection PyBroadException
+    try:
+        installed_version = software_module.installed_version()
+    except Exception:
+        installed_version = 'Unknown'
+    # noinspection PyBroadException
+    try:
+        latest_version = software_module.latest_version()
+    except Exception:
+        latest_version = 'Unknown'
     print('Software: %s' % name)
     print('    Installed Version : %s' % installed_version)
     print('    Latest Version    : %s' % latest_version)
     if (installed_version is not None) and (installed_version != latest_version):
-        print('    An upgrade is available.')
+        if installed_version == 'Unknown' or latest_version == 'Unknown':
+            print('    Check manually if an upgrade is available.')
+        else:
+            print('    An upgrade is available.')
 
 
 def main():
-    for version_info in all_versions():
-        print_version_results(version_info)
+    for m in all_modules(versions.software.__path__):
+        if is_software_module(m):
+            print_version_results(m)
 
 
 if __name__ == '__main__':
